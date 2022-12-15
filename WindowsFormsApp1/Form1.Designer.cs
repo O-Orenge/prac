ebeb970055846f160221f5bc7230c0906d7ee99f
O-Orenge <53816230+O-Orenge@users.noreply.github.com>
2022-12-15 19:16:56 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace zadanie
+{
+    partial class Task
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.distance = new System.Windows.Forms.TextBox();
+            this.speed = new System.Windows.Forms.TextBox();
+            this.time = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.оАвтореToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.инфоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.обАвтореToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.какРаботатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timemin = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Click1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Рассчёт времени в пути";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Расстояние в км: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Скорость в км/ч: ";
+            // 
+            // distance
+            // 
+            this.distance.Location = new System.Drawing.Point(111, 55);
+            this.distance.Name = "distance";
+            this.distance.Size = new System.Drawing.Size(100, 20);
+            this.distance.TabIndex = 6;
+            // 
+            // speed
+            // 
+            this.speed.Location = new System.Drawing.Point(111, 94);
+            this.speed.Name = "speed";
+            this.speed.Size = new System.Drawing.Size(100, 20);
+            this.speed.TabIndex = 7;
+            // 
+            // time
+            // 
+            this.time.Location = new System.Drawing.Point(241, 90);
+            this.time.Name = "time";
+            this.time.ReadOnly = true;
+            this.time.Size = new System.Drawing.Size(45, 20);
+            this.time.TabIndex = 8;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оАвтореToolStripMenuItem,
+            this.инфоToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(434, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // оАвтореToolStripMenuItem
+            // 
+            this.оАвтореToolStripMenuItem.Name = "оАвтореToolStripMenuItem";
+            this.оАвтореToolStripMenuItem.Size = new System.Drawing.Size(12, 20);
+            // 
+            // инфоToolStripMenuItem
+            // 
+            this.инфоToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.обАвтореToolStripMenuItem,
+            this.какРаботатьToolStripMenuItem});
+            this.инфоToolStripMenuItem.Name = "инфоToolStripMenuItem";
+            this.инфоToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.инфоToolStripMenuItem.Text = "Инфо";
+            // 
+            // обАвтореToolStripMenuItem
+            // 
+            this.обАвтореToolStripMenuItem.Name = "обАвтореToolStripMenuItem";
+            this.обАвтореToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.обАвтореToolStripMenuItem.Text = "Об авторе";
+            this.обАвтореToolStripMenuItem.Click += new System.EventHandler(this.Clickauth);
+            // 
+            // какРаботатьToolStripMenuItem
+            // 
+            this.какРаботатьToolStripMenuItem.Name = "какРаботатьToolStripMenuItem";
+            this.какРаботатьToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.какРаботатьToolStripMenuItem.Text = "Как работать";
+            this.какРаботатьToolStripMenuItem.Click += new System.EventHandler(this.clickwhy);
+            // 
+            // timemin
+            // 
+            this.timemin.Location = new System.Drawing.Point(334, 90);
+            this.timemin.Name = "timemin";
+            this.timemin.ReadOnly = true;
+            this.timemin.Size = new System.Drawing.Size(45, 20);
+            this.timemin.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(292, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "часов";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(385, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "минут";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(238, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Результат:";
+            // 
+            // Task
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 197);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.timemin);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.speed);
+            this.Controls.Add(this.distance);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Task";
+            this.Text = "Вариант №4";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox distance;
+        private System.Windows.Forms.TextBox speed;
+        private System.Windows.Forms.TextBox time;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem оАвтореToolStripMenuItem;
+        private System.Windows.Forms.TextBox timemin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ToolStripMenuItem инфоToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem обАвтореToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem какРаботатьToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace zadanie
+{
+    public partial class Task : Form
+    {
+        public Task()
+        {
+            InitializeComponent();
+        }
+        private void Click1(object sender, EventArgs e)
+        {
+            time.Text = " ";
+            timemin.Text = " ";
+            string path = @"C:\Users\maste\Desktop\zadanie\sum.txt";
+            decimal a, b, c, d, f, t = 3600;
+            if (Convert.ToDecimal(distance.Text) == 0 | Convert.ToDecimal(speed.Text) == 0)
+            {
+                time.Text = "Ошибка!";
+                timemin.Text = "Ошибка!";
+                using (FileStream file = new FileStream(path, FileMode.Append))
+                using (StreamWriter stream = new StreamWriter(file))
+                    stream.WriteLine($"{distance.Text} км : {speed.Text} км/ч = Ошибка!");
+                return;
+            }
+            else
+                a = Convert.ToDecimal(distance.Text) * 1000;
+            b = Convert.ToDecimal(speed.Text) * 5;
+            f = a * 18 / b / t;
+            c = (decimal)Math.Floor(a * 18 / b / t);
+            d = f - c;
+            d = (decimal)Math.Round(d * 60);
+            time.Text = Convert.ToString(c);
+            timemin.Text = Convert.ToString(d);
+            using (FileStream file = new FileStream(path, FileMode.Append))
+            using (StreamWriter stream = new StreamWriter(file))
+            {
+                if (c < 1)
+                    stream.WriteLine($"{distance.Text} км : {speed.Text} км/ч = {time.Text} часов и {timemin.Text} минут");
+                else if (c == 1)
+                    stream.WriteLine($"{distance.Text} км : {speed.Text} км/ч = {time.Text} час и {timemin.Text} минут");
+                else if (c > 5)
+                    stream.WriteLine($"{distance.Text} км : {speed.Text} км/ч = {time.Text} часов и {timemin.Text} минут");
+                else
+                    stream.WriteLine($"{distance.Text} км : {speed.Text} км/ч = {time.Text} часа и {timemin.Text} минут");
+                return;
+            }
+
+        }
+        private void Clickauth(object sender, EventArgs e)
+        {
+            MessageBox.Show("Студент Нечаев Илья,\nгруппа ИСП-320А.", "Автор", MessageBoxButtons.OK);
+        }
+
+        private void clickwhy(object sender, EventArgs e)
+        {
+            MessageBox.Show("Необходимо заполнить поля <<Расстояние>> и <<Скорость>>,\nдалее необходимо нажать кнопку 'Рассчитать' и\nвы получите результат.\nОставлять пустые поля нельзя, при использовании десятых и далее,\nписать нужно через запятую!", "Как работать", MessageBoxButtons.OK);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
